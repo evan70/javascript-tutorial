a8dce9d187bffc871bca70aca33a0efee2e87141
evan70 <ivanpaldan@yandex.com>
2021-10-30 21:25:08 +0200

    modified:   10. Comfy Store 19/products.html 	modified:   10. Comfy Store 19/src/filters/search.js 	modified:   10. Comfy Store 19/src/pages/products.js

--- 10. Comfy Store 19/src/pages/products.js
+++ 10. Comfy Store 19/src/pages/products.js
@@ -4,8 +4,8 @@ import '../cart/toggleCart.js';
 import '../cart/setupCart.js';
 
 //  filter imports
-import setupSearch from '../filters/search.js';
 import setupCompanies from '../filters/companies.js';
+import setupSearch from '../filters/search.js';
 import setupPrice from '../filters/price.js';
 
 // specific imports
@@ -13,9 +13,4 @@ import { store } from '../store.js';
 import display from '../displayProducts.js';
 import { getElement } from '../utils.js';
 
-const loading = getElement('.page-loading');
-
 display(store, getElement('.products-container'));
-
-setupSearch(store);
-loading.style.display = 'none';
