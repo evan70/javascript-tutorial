3cc6c657daafac9ee728b1f255b79260891aa98b
evan70 <ivanpaldan@yandex.com>
2021-11-21 22:19:27 +0100

    modified:   10. Comfy Store 19/src/cart/setupCart.js

--- 10. Comfy Store 19/src/cart/setupCart.js
+++ 10. Comfy Store 19/src/cart/setupCart.js
@@ -58,6 +58,9 @@ function displayCartItemsDOM() {
     addToCartDOM(cartItem);
   });
 }
+function removeItem(id) {
+  cart = cart.filter((cartItem) => cartItem.id !== id);
+}
 function increaseAmount(id) {
   let newAmount;
   cart = cart.map((cartItem) => {
@@ -69,7 +72,25 @@ function increaseAmount(id) {
   });
   return newAmount;
 }
-function setupCartFunctionality() {}
+function setupCartFunctionality() {
+  cartItemsDOM.addEventListener('click', function (e) {
+    const element = e.target;
+    const parent = e.target.parentElement;
+    const id = e.target.dataset.id;
+    const parentID = e.target.parentElement.dataset.id;
+    // remove
+    if (element.classList.contains('cart-item-remove-btn')) {
+      removeItem(id);
+      // parent.parentElement.remove();
+      element.parentElement.parentElement.remove();
+    }
+    // increase
+    // decrease
+    displayCartItemCount();
+    displayCartTotal();
+    setStorageItem('cart', cart);
+  });
+}
 const init = () => {
   // display amount of cart items
   displayCartItemCount();
