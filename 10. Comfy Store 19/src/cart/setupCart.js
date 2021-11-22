6787ef0909b721d59394a4254cc577a1b8031029
evan70 <ivanpaldan@yandex.com>
2021-11-22 22:54:36 +0100

    modified:   10. Comfy Store 19/src/cart/setupCart.js

--- 10. Comfy Store 19/src/cart/setupCart.js
+++ 10. Comfy Store 19/src/cart/setupCart.js
@@ -84,6 +84,10 @@ function setupCartFunctionality() {
       // parent.parentElement.remove();
       element.parentElement.parentElement.remove();
     }
+    if (parent.classList.contains('cart-item-increase-btn')) {
+      const newAmount = increaseAmount(parentID);
+      parent.nextElementSibling.textContent = newAmount;
+    }
     // increase
     // decrease
     displayCartItemCount();
